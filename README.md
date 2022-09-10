bb2b99e89749cd54c042ba0df976d43279ef3fb4
LarissaCojocaru <106817613+LarissaCojocaru@users.noreply.github.com>
2022-09-10 15:15:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-crez4p-jjuonp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-crez4p-jjuonp)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+};
+
